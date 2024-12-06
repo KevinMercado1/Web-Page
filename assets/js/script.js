d89048d65136d11042be84f01d1cab20d0818120
Esau Ortiz <117240048+esadortiz@users.noreply.github.com>
2024-12-05 19:55:57 -0500

    actualizacion

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,14 @@ let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
 // Manejo de productos dinámico
 const products = [
-  { id: 1, name: "Laptop", price: 800, img: "pantalla-de-portatil.png" },
-  { id: 2, name: "Smartphone", price: 600, img: "smartphone.jpg" },
-  { id: 3, name: "Sofá", price: 300, img: "sofa.jpg" },
-  { id: 4, name: "Lámpara", price: 50, img: "lamp.jpg" },
-  { id: 5, name: "Bicicleta", price: 150, img: "bike.jpg" },
-  { id: 6, name: "Balón de fútbol", price: 30, img: "football.jpg" },
+  { id: 1, name: 'collar + aretes', price: 800, img: 'cadena+aretes.jpg' },
+  { id: 2, name: 'Aretes Redondos', price: 600, img: 'artes2.jpg' },
+  { id: 3, name: 'Reloj 1.1', price: 300, img: 'Reloj2.jpg' },
+  { id: 4, name: 'Relojes de cuero', price: 50, img: 'Reloj3.jpeg' },
+  { id: 5, name: 'Manilla 1.1', price: 150, img: 'manilla2.jpg' },
+  { id: 6, name: 'Manillas con perlas', price: 30, img: 'manilla3.jpg' },
 ];
+  
 
 // Agregar producto al carrito
 document.querySelectorAll('.add-to-cart').forEach(button => {
